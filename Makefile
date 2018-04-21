20bd77debc4942a2ad274655da3a281eb4aabe5a
Sourav Mukherjee <sourav01mukherjee@gmail.com>
2018-04-21 11:19:06 +0530

    Improve code formatting of Tutorial 3; update Makefile

--- Makefile
+++ Makefile
@@ -5,9 +5,11 @@
 INCLUDE_PATH=${SFML_DIR}/include
 LIBRARY_PATH=${SFML_DIR}/lib
 LIBRARIES=-lsfml-system -lsfml-window -lsfml-graphics
-IMAGE_tank=Tutorial\ 2/build-untitled1-Desktop_Qt_5_7_0_GCC_64bit-Debug/tank.png
 
-all: out/t1.out t2
+IMAGE_tank=Tutorial\ 2/build-untitled1-Desktop_Qt_5_7_0_GCC_64bit-Debug/tank.png
+IMAGE_SpriteSheet=Tutorial\ 3/build-Tutorial3-Desktop_Qt_5_7_0_GCC_64bit-Debug/SpriteSheet.png
+
+all: out/t1.out t2 t3
 
 out/t1.out: Tutorial\ 1/Tutorial1/main.cpp
 	g++ -std=c++17 -o out/t1.out "Tutorial 1/Tutorial1/main.cpp" \
@@ -23,4 +25,16 @@ out/t2.out: Tutorial\ 2/Tutorial2/main.cpp
 	-L$(LIBRARY_PATH) $(LIBRARIES)
 
 out/tank.png: $(IMAGE_tank)
-	cp $(IMAGE_tank) out/  
+	cp $(IMAGE_tank) out/
+
+# For running "Tutorial 3", out/t3.out and out/SpriteSheet.png are needed.
+t3: out/t3.out out/SpriteSheet.png
+
+out/t3.out: Tutorial\ 3/Tutorial3/main.cpp
+	g++ -std=c++17 -o out/t3.out "Tutorial 3/Tutorial3/main.cpp" \
+	-I$(INCLUDE_PATH) \
+	-L$(LIBRARY_PATH) $(LIBRARIES)
+
+out/SpriteSheet.png: $(IMAGE_SpriteSheet)
+	cp $(IMAGE_SpriteSheet) out/
+
--- Tutorial 3/Tutorial3/main.cpp
+++ Tutorial 3/Tutorial3/main.cpp
@@ -5,42 +5,60 @@
 #include <ctime>
 #include <stdlib.h>
 
-#define WINDOW_WIDTH 600
-#define WINDOW_HEIGHT 600
+constexpr unsigned int WINDOW_WIDTH {600}, WINDOW_HEIGHT {600};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My first SFML game", sf::Style::Default);
-    sf::Vector2u size(400, 400);
+    sf::RenderWindow window(
+        sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+        "My first SFML game", sf::Style::Default);
+
+    const sf::Vector2u size(400, 400);
+
     window.setSize(size);
+
     window.setTitle("New title");
+
     window.setPosition(sf::Vector2i(100, 100));
+
     enum Direction {DOWN, LEFT, RIGHT, UP};
+
     //int sourceX = 32, sourceY = DOWN;
     float frameCounter = 0, frameSpeed = 500, frameSwitch = 100;
+
     sf::Clock clock;
+
     bool updateFrame = true;
+
     sf::Vector2i source(0, DOWN);
-    sf::Vector2i pSize(51, 55);
+
+    const sf::Vector2i pSize(51, 55);
+
     sf::Texture pTexture;
+
     sf::Sprite pSprite;
 
     if(!pTexture.loadFromFile("SpriteSheet.png"))
     {
         std::cout << "Could not load image" << std::endl;
     }
+
     //pSprite.setRotation(90);
     pSprite.setTexture(pTexture);
     pSprite.setPosition(50, 140);
+
     //std::cout << "width = " << pTexture.getSize().x << std::endl;
     //std::cout << "height = " << pTexture.getSize().y << std::endl;
+
     /*srand(time(NULL));
     int delay = 30;
     int counter = 0;
     float angle = 0.0f;*/
+
     while(window.isOpen())
     {
         sf::Event event;
+
         while(window.pollEvent(event))
         {
             switch(event.type)
@@ -48,15 +66,21 @@ int main()
                 case sf::Event::Closed :
                     window.close();
                     break;
+
                 case sf::Event::KeyPressed :
                     if(event.key.code == sf::Keyboard::Escape)
+                    {
                         window.close();
+                    }
                     break;
+
                 default :
                     break;
             }
         }
+
         //Обработка нажатий клавиш вне цикла обработки очереди событий
+        //(Google translate) Handling keystrokes outside the event queue processing loop
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
             source.y = UP;
@@ -66,7 +90,7 @@ int main()
         {
             source.y = DOWN;
             pSprite.move(0, 1);
-         }
+        }
         else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
             source.y = LEFT;
@@ -77,10 +101,13 @@ int main()
             source.y = RIGHT;
             pSprite.move(1, 0);
         }
+
         //Обработка нажатий клавиш мыши вне очереди обработки событий
+        //(Google translate) Handling mouse clicks outside the event processing queue
         sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
         sf::Vector2f playerPosition = pSprite.getPosition();
         //std::cout << "Mouse postion - X: " << mousePosition.x << " Y: " << mousePosition.y << std::endl;
+
         //Включаем анимацию персонажа ЛКМ и отключаем ее ПКМ
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
@@ -91,6 +118,7 @@ int main()
             updateFrame = false;
         }
         //Передвижение персонажа с помощью мышки
+        //(Google translate) Move the character with the mouse
         if(mousePosition.x < playerPosition.x + 25)
         {
             source.y = LEFT;
@@ -111,11 +139,20 @@ int main()
             source.y = DOWN;
             pSprite.move(0, 1);
         }
-        std::cout << "player position x = " << playerPosition.x << " y = " << playerPosition.y << std::endl;
-        std::cout << "mouse position x = " << mousePosition.x << " y = " << mousePosition.y << std::endl;
+
+        std::cout << "player position x = " << playerPosition.x
+            << " y = " << playerPosition.y << std::endl;
+
+        std::cout << "mouse position x = " << mousePosition.x
+            << " y = " << mousePosition.y << std::endl;
+
         //Для одинаковой скорости анимации персонажа на компьютерах разной мощности
         //Ноутбук выполняет игровой цикл каждые 16 милисекунд
-        frameCounter = updateFrame ? frameCounter + frameSpeed * clock.restart().asSeconds() : 0;
+        //(Google translate) For the same speed of character animation on computers
+        //of different power the laptop performs the game cycle every 16 milliseconds
+        frameCounter = updateFrame ? frameCounter +
+            frameSpeed * clock.restart().asSeconds() : 0;
+
         if(frameCounter >= frameSwitch)
         {
             source.x += 1;
@@ -134,10 +171,15 @@ int main()
                 angle = 0;
             pSprite.setRotation(angle);
         }*/
-        pSprite.setTextureRect(sf::IntRect(source.x * pSize.x, source.y * pSize.y, pSize.x, pSize.y));
+
+        pSprite.setTextureRect(
+            sf::IntRect(
+                source.x * pSize.x, source.y * pSize.y, pSize.x, pSize.y));
+
         window.clear();
         window.draw(pSprite);
         window.display();
     }
+
     return 0;
 }
